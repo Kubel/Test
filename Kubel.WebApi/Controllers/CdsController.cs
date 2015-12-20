7b0861076b6fbf3ad9000e3433dfec51b753eeb6
Kubel <poulkubel@gmail.com>
2015-12-20 06:57:52 +0100

    Added CD section

---
+++ Kubel.WebApi/Controllers/CdsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Kubel.WebApi.Controllers
+{
+    public class CdsController : ApiController
+    {
+        // GET api/cds
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/cds/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/cds
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/cds/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/cds/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Kubel.WebApi/Models/CD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kubel.WebApi.Models
+{
+    public class CD
+    {
+        public int ID { get; set; }
+        public string Artist { get; set; }
+        public string Title { get; set; }
+        public string Country { get; set; }
+        public string Label { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+    }
+}
